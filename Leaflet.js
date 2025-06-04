95fb9aa48a844402027dbf5f78bedcafe1ccf6d9
sualpfb <sualpkula@gmail.com>
2025-06-04 20:40:41 +0300

    Add files via upload

---
+++ Leaflet.js
@@ -0,0 +1,8 @@
+<!-- Leaflet CSS -->
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+/>
+
+<!-- Leaflet JS -->
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
